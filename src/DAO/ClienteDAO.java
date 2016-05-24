b06342d632b1de0384f1a2db9097aaf9c03853bf
ianka <ianka@ianka-Aspire-E5-571G>
2016-05-24 12:02:35 -0300

    new commit

---
+++ src/DAO/ClienteDAO.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import java.sql.*;
+import Modelo.Cliente;
+
+/**
+ *
+ * @author ianka
+ */
+public class ClienteDAO extends ExecuteSQL{
+
+    public ClienteDAO(Connection con) {
+        super(con);
+    }
+    
+    public String Inserir_Cliente(Cliente a){
+        String sql = "insert into cliente values(0,?,?,?,?,?,?,?,?,?,?)";
+        try {
+                PreparedStatement ps = getCon().prepareStatement(sql);
+                
+                ps.setString(1, a.getNome());
+                ps.setString(2, a.getNascimento());
+                ps.setString(3, a.getRG());
+                ps.setString(4, a.getCPF());
+                ps.setString(5, a.getEmail());
+                ps.setString(6, a.getTelefone());
+                ps.setString(7, a.getBairro());
+                ps.setString(8, a.getRua());
+                ps.setInt(9, a.getNumero());
+                ps.setString(10, a.getCEP());
+                
+                if(ps.executeUpdate() > 0){
+                    return "Inserido com Sucesso.";
+                }else{
+                    return "Erro ao inserir.";
+                }
+        } catch (SQLException e) {
+            return e.getMessage();
+        }
+    }
+    
+}
